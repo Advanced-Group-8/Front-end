229c45fd57a4eba758d0d8c93db4ad1c2abac40e
Tobias Engvall <tobias.engvall@chasacademy.se>
2025-09-05 11:48:00 +0200

    basic setup done, made some folders, installed and configured tailwind, added colorpalette,

---
+++ web-app/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="bg-accent-4 text-accent-1 flex text-center m-auto h-screen items-center justify-center text-4xl font-bold">
+      Home
+    </div>
+  );
+};
+
+export default Home;
