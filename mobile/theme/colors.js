61df4fcf961cd5fd4231aa4b327a346974126ad2
Erik Torres Puente <Erik.torres.puente@gmail.com>
2025-09-30 03:02:43 +0200

    Introduced a ThemeContext, ThemeProvider, useTheme hook and StatusBar updates. Defined light/dark themes in colors.js with custom color palettes using buildCustomTheme

---
+++ mobile/theme/colors.js
@@ -0,0 +1,78 @@
+// colors.js
+
+// LIGHT THEME
+export const lightTheme = {
+  // Primary colors
+  primary: "#26408B",   // blå
+  secondary: "#507255", // grön
+
+  // Accent colors
+  accentGreen: "#ABDBB5",
+  accentOrange: "#FFA630",
+  accentRed: "#D32F2F",
+  accentMutedGreen: "#507255",
+
+  // Neutrals
+  backgroundPrimary: "#EDF4F4",  // ljus bakgrund
+  backgroundSecondary: "#FFFFFF",
+  surface: "#F5F5F5",
+  border: "#1C1C1C",
+
+  // Text & icons
+  textPrimary: "#000000",
+  textSecondary: "#333333",
+  textAccentPrimary: "#26408B",
+  textAccentSecondary: "#507255",
+  iconAccent: "#000000",
+
+  // Components
+  headerBackground: "#EDF4ED",
+  tabBar: "#FFFFFF",
+  tabBarIcon: "#26408B",
+  tabBarIconActive: "#FFA630",
+  themeButton: "#507255",
+  modalBackground: "#FFFFFF",
+  
+  // Feedback
+  notification: "#D32F2F",
+  shadow: "#00000029",
+};
+
+
+// DARK THEME
+export const darkTheme = {
+  // Primary colors
+  primary: "#26408B",   // blå
+  secondary: "#507255", // grön
+
+  // Accent colors
+  accentGreen: "#ABDBB5",
+  accentOrange: "#FFA630",
+  accentRed: "#D32F2F",
+  accentMutedGreen: "#507255",
+
+  // Neutrals
+  backgroundPrimary: "#1C1C1C",   // mörk bakgrund
+  backgroundSecondary: "#000000",
+  surface: "#121212",
+  border: "#333333",
+
+  // Text & icons
+  textPrimary: "#EDF4ED",
+  textSecondary: "#9E9E9E",
+  textAccentPrimary: "#ABDBB5",
+  textAccentSecondary: "#FFA630",
+  iconAccent: "#FFFFFF",
+
+  // Components
+  headerBackground: "#000000",
+  tabBar: "#1C1C1C",
+  tabBarIcon: "#ABDBB5",
+  tabBarIconActive: "#FFA630",
+  themeButton: "#507255",
+  modalBackground: "#1C1C1C",
+  
+  // Feedback
+  notification: "#D32F2F",
+  shadow: "#00000099",
+};
