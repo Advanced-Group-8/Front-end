e2bf50cb42ad5478fd58c368e9ac1e6a9dd80a8a
Erik Torres Puente <Erik.torres.puente@gmail.com>
2025-10-20 05:26:14 +0200

    Bottom navigation now instead reflects five tab screens, of which one tab screen is for QR scan code. It has been designed via additional function with style code within a touchableopacity inside navigation.js

---
+++ mobile/screens/QRScannerScreen.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useTheme } from "../theme/ThemeContext";
+
+const QRScannerScreen = () => {
+  const { theme } = useTheme();
+  const styles = createStyles(theme);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>QR Scanner Screen</Text>
+    </View>
+  );
+};
+
+const createStyles = (theme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: theme.backgroundPrimary,
+    },
+    text: {
+      color: theme.textPrimary,
+      fontSize: 20,
+    },
+  });
+
+export default QRScannerScreen;
