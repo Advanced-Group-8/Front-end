df086bacf061c959012bb1f100b79955d88804f4
bexgren <rebecka.gren@gmail.com>
2025-11-03 12:59:45 +0100

    added a comment to getPackageStatus

--- mobile/utils/getPackStatus.ts
+++ mobile/utils/getPackStatus.ts
@@ -4,6 +4,7 @@ import getPackageById from "./fetch/getPackageById";
 export const getPackStatus = (id: number, token: string) => {
   const pack = getPackageById(url, id, token);
   if (pack) {
+    // the line below works even if it's marked red
     const packStatus = pack.status;
     return packStatus;
   }
