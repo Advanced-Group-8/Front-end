95b54614565ef68d4c5c6991a968f53120da2ee1
bexgren <rebecka.gren@gmail.com>
2025-11-03 11:54:33 +0100

    made some changes to getPackageById.ts

--- mobile/utils/fetch/getPackageById.ts
+++ mobile/utils/fetch/getPackageById.ts
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // BEWARE: don't know if any of this works...
 const getPackageById = (url: string, id: number) => {
+  const [pack, setPack] = useState<object | null>(null);
+  const [error, setError] = useState<string | null>(null);
   useEffect(() => {
-    const getSinglePackage = async (url: string, id: number) => {
+    const getSinglePackage = async () => {
       try {
         const response = await fetch(`${url}/package/{${id}}`, {
           method: "GET",
@@ -11,18 +13,24 @@ const getPackageById = (url: string, id: number) => {
           },
         });
 
-        if (!response.ok) throw new Error("Error getting user");
+        if (!response.ok) throw new Error("Error getting package");
 
-        const data: object = await response.json(); //returns profile?
-        return data;
-      } catch (error) {
-        console.error("Error getting user", error);
-        return null;
+        const data = await response.json(); //returns profile?
+        setPack(data.data);
+        console.log(pack);
+      } catch (err) {
+        console.error("Error getting package", err);
+        setError(`Failed connection with server`);
       }
     };
 
-    getSinglePackage(url, id);
-  }, []);
+    getSinglePackage();
+  }, [url, id]);
+  if (error) {
+    return null;
+  } else {
+    return { pack };
+  }
 };
 
 export default getPackageById;
